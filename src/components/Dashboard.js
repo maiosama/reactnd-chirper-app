c505e71cf3d6472692a6106f6001a95aa57b4034
maiosama <72415874+maiosama@users.noreply.github.com>
2021-05-09 00:09:18 +0200

    Add files via upload

---
+++ src/components/Dashboard.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import Tweet from './Tweet'
+
+class Dashboard extends Component {
+    render (){
+        return(
+            <div>
+                <h3 className = 'center'> Your Timeline </h3>
+                <ul className = 'dashboard-list'>
+                    {this.props.tweetIds.map((id)=> (
+                        <li key ={id}>
+                            <Tweet id = {id}/>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProp ({ tweets}){
+    return {
+        tweetIds: Object.keys(tweets)
+        .sort ((a,b) => tweets[b].timeStamp -tweets[a].timeStamp)
+    }
+}
+
+export default connect(mapStateToProp)(Dashboard)
